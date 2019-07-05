2b279ca2d8f4bb21efd7cf1deff3c84d0ccdb592
Shivam Jha <shivi1685@gmail.com>
2019-07-05 18:38:45 +0530

    MY INTERNSHIP PROJECT

---
+++ models/Products.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  productName: {
+    type: String,
+    required: true
+  },
+  productSerialNo: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  pricePerUnit: {
+    type: Number,
+    required: true
+  },
+  vendorName:{
+    type:String,
+    required: true
+  }
+});
+
+mongoose.model("products", ProductSchema);
